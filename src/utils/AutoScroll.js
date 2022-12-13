00cc9eb0931b7d5e5ac3829831f010eb9ff94800
sugiartofahmi <sugiartofahmi@gmail.com>
2022-12-13 18:29:24 +0700

    Fahmi Sugiarto | Create Module

---
+++ src/utils/AutoScroll.js
@@ -0,0 +1,18 @@
+export const AutoScroll = async (page) => {
+  await page.evaluate(async () => {
+    await new Promise((resolve, reject) => {
+      let totalHeight = 0;
+      let distance = 100;
+      let timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+};
